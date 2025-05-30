f40768c9fa2164501e63e093ceb66eb31e154593
KimJooyoung <kimjy2000117@gmail.com>
2025-05-31 02:45:26 +0900

    ✨ 회원가입 및 로그아웃 구현

--- src/components/kakaoMap/KakaoMap.jsx
+++ src/components/kakaoMap/KakaoMap.jsx
@@ -67,7 +67,9 @@ export default function KakaoMap() {
               box-shadow: 0 4px 12px rgba(0,0,0,0.25);
               font-size: 13px;
               line-height: 1.5;
-              position: relative;
+              position: absolute;
+              top: -360px;
+              right: -150px;
             ">
               <button class="close-overlay-btn" style="
                 position: absolute;
--- src/pages/signup/SignUpPage.jsx
+++ src/pages/signup/SignUpPage.jsx
@@ -1,8 +1,10 @@
 import styles from './SignUpPage.module.css'
+import SignUpForm from '../../components/signup/SignUpForm';
 
 export default function SignUpPage(){
     return(
-        <>
-        </>
+        <div className={styles.signUpContainer}>
+            <SignUpForm/>
+        </div>
     );
 }
